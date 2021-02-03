37b62a3dc9cab0e494a1bab075dfa4a29d94a357
Zephyr Worthington <zephyr@Zephyrs-MacBook-Pro.local>
2021-02-03 12:08:07 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Screen
+import time
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+game_on = True
+
+while game_on:
+    time.sleep(0.1)
+    screen.update()
